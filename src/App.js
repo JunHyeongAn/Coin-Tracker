f04e948731b41983f7ea592e800e27f4ae59e030
Jun Hyeong An <99971427+JunHyeongAn@users.noreply.github.com>
2022-02-20 16:05:12 +0900

    coin tracker
    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [btc, setBtc] = useState(0);
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  }
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(resp => resp.json())
+    .then(json => {
+      setCoins(json);
+      
+      json.forEach(element => {
+        if(element.id === "btc-bitcoin") {
+          setBtc(element.quotes.USD.price);
+          return;
+        }
+      });
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>The Coins!</h1>
+        {loading ? <strong>loading...</strong> : 
+          <select>
+            {coins.map(coin => 
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+              </option>
+            )}
+          </select>
+        }
+        
+        <h3>how many BTC you can buy!!</h3>
+        <input onChange={onChange} value={money} type="number" placeholder="how much can you spend"/>
+        $
+        <div>You can buy {money / btc} BTC</div>
     </div>
   );
 }
